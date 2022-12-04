ec782583aef80fb21058e1303954ac7b74a102c7
a.svintsitskiy@zmeke.com <a.svintsitskiy@zmeke.com>
2022-12-04 21:01:01 +0300

    feat: add solve fs:delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
-const remove = async () => {
-    // Write your code here 
+import * as fsp from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
+export const remove = async () => {
+  try {
+    await fsp.access(path.join(__dirname, "files/fileToRemove.txt"));
+    await fsp.unlink(path.join(__dirname, "files/fileToRemove.txt"));
+  } catch (error) {
+    console.log(`FS operation failed`);
+  }
 };
 
-await remove();
+remove();
