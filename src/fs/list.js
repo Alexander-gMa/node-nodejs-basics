5f6b7ea00ed2edf012a63f1d434958346c5d68b8
a.svintsitskiy@zmeke.com <a.svintsitskiy@zmeke.com>
2022-12-04 21:17:46 +0300

    feat: add solve fs:list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,20 @@
-const list = async () => {
-    // Write your code here 
+import fs from "fs";
+import * as fsp from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
+export const list = async () => {
+  try {
+    await fsp
+      .readdir(path.join(__dirname, "files"))
+      .then((res) => console.log(res));
+  } catch (error) {
+    console.log(`FS operation failed`);
+  }
 };
 
-await list();
+list();
