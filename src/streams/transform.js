719303cc4458515b991dab9245856867aeece318
a.svintsitskiy@zmeke.com <a.svintsitskiy@zmeke.com>
2022-12-05 22:57:10 +0300

    feat: add solve fs:stream

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,14 @@
-const transform = async () => {
-    // Write your code here 
+import { stdin, stdout } from "node:process";
+import { Transform } from "stream";
+
+export const transform = async () => {
+  const transform = new Transform({
+    transform: (chunk, _, callback) => {
+      const reverse = chunk.reverse();
+      callback(null, `${reverse}\n`);
+    },
+  });
+  stdin.pipe(transform).pipe(stdout);
 };
 
-await transform();
+transform();
